fc4d84d185f62b75ee65767b4524d562d13c7d82
Mlink6783 <mlink6783@gmail.com>
2025-04-21 00:04:43 +0600

    Update App.jsx

--- modding-frontend/src/App.jsx
+++ modding-frontend/src/App.jsx
@@ -3,6 +3,14 @@ import Home from "./pages/Home";
 import ModDetail from "./pages/ModDetail";
 import AddMod from "./pages/AddMod";
 import Navbar from "./components/Navbar";
+import { getMods, getMod, addMod } from "../api";
+
+useEffect(() => {
+  getMods()
+    .then((res) => console.log(res.data))
+    .catch((err) => console.error(err));
+}, []);
+
 
 export default function App() {
   return (
