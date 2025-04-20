f7fdc9b6401c9020fa6f34eda7416bd5a5a5f8ab
Mlink6783 <mlink6783@gmail.com>
2025-04-21 00:03:25 +0600

    Update api.js

--- modding-frontend/src/api.js
+++ modding-frontend/src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_BASE = "https://your-backend.onrender.com/api";
+const API_BASE = "https://modding-backend.onrender.com/api";
+
+export default API_BASE;
+
 
 export const getMods = () => axios.get(`${API_BASE}/mods`);
 export const getMod = (id) => axios.get(`${API_BASE}/mods/${id}`);
